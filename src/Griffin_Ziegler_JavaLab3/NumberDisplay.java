a4ca33ac785b5e1ad3300b1715e83964235bd6f0
griffzig <98347610+griffzig@users.noreply.github.com>
2023-09-12 16:06:18 -0500

    Initial commit

---
+++ src/Griffin_Ziegler_JavaLab3/NumberDisplay.java
@@ -0,0 +1,25 @@
+package Griffin_Ziegler_JavaLab3;
+
+import javax.swing.*;
+import java.awt.*;
+
+// Displays a numeric entry pad
+
+public class NumberDisplay {
+
+	public static void main(String[] args) {
+		// create entry pad container
+		JFrame ep = new JFrame();
+		
+		ep.setSize(400, 400);
+		ep.getContentPane().setBackground(Color.BLACK);
+		
+		
+		
+		
+		ep.setVisible(true);
+		
+	}
+
+	// imageIcon
+}

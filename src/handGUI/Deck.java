49f0a1e95924abaeed54fea66cbb521ec39c98cc
griffzig <98347610+griffzig@users.noreply.github.com>
2023-09-22 12:01:36 -0500

    Added card, hand, and deck classes. Added rank and suit enum

--- src/handGUI/Deck.java
+++ src/handGUI/Deck.java
@@ -1,5 +1,74 @@
 package handGUI;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+// represents a full deck of 52 playing cards
+
 public class Deck {
 
+	private List<Card> cards; // total cards
+	private List<Card> dealtCards; // cards drawn by dealer
+	
+	public Deck() {
+		
+		addDeck();
+	}
+	
+	// initialize the deck
+	private void addDeck() {
+		
+		cards = new ArrayList<>();
+		dealtCards = new ArrayList<>();
+		
+		for (Suit suit : Suit.values()) {
+			for (Rank rank : Rank.values()) {
+				cards.add(new Card(rank, suit));
+			}
+		}
+	}
+	
+	// draw a random card from the deck
+	public Card draw() {
+		
+		if (cards.isEmpty()) {
+			return null; // if deck is empty, cannot draw
+		
+		} else {	
+			Random rand = new Random();
+			int i = rand.nextInt(cards.size());
+			Card drawCard = cards.remove(i);
+			dealtCards.add(drawCard);
+			
+			return drawCard;
+		}
+	}
+	
+	// deals a certain number of random cards
+	public List<Card> deal(int totalCards) {
+		
+		List<Card> dealt = new ArrayList<>();
+		
+		for (int i = 0; i < totalCards; i++) {
+			Card card = draw();
+			
+			if (card != null) {
+				dealt.add(card);
+			
+			} else {
+				break; 
+			}
+		}
+		
+		return dealt;
+	}
+	
+	// return deck to original state
+	public void renewDeck() {
+		
+		cards.addAll(dealtCards);
+		dealtCards.clear();
+	}
 }
+
---
+++ src/handGUI/Suit.java
@@ -0,0 +1,9 @@
+package handGUI;
+
+// represents different card suits
+public enum Suit {
+	CLUBS,
+	DIAMONDS,
+	HEARTS,
+	SPADES;
+}
